71cc4e686ec668d7d0fdb2c6eb9fca6fbc5dbdb6
shivgirja <amitsrivastava115@yahoo.com>
2022-01-28 14:42:07 +0530

    Part 1 complete

---
+++ Flashzilla/Model/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Amit Shrivastava on 28/01/22.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
---
+++ Flashzilla/Views/Flashzilla.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Amit Shrivastava on 26/01/22.
+//
+
+import SwiftUI
+import CoreHaptics
+
+struct Flashzilla: View {
+    @State private var cards = [Card](repeating: Card.example, count: 10)
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+                VStack {
+                    ZStack {
+                        ForEach(0..<cards.count, id: \.self) { index in
+                            CardView(card: cards[index]) {
+                                withAnimation {
+                                    removeCard(at: index)
+                                }
+                                
+                            }
+                                .stacked(at: index, in: cards.count)
+                        }
+                    }
+                }
+        }
+    }
+    
+    func removeCard(at position: Int) {
+        cards.remove(at: position)
+    }
+ 
+}
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(x: 0, y: offset * 10)
+    }
+}
+
+struct Flashzilla_Previews: PreviewProvider {
+    static var previews: some View {
+        Flashzilla()
+    }
+}
